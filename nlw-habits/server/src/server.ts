a485dbdb5ba2b466eed3f8dccee733181c909b01
lucaslomba <lucasazevedolomba@gmail.com>
2023-01-16 23:49:24 -0300

    Finalizando aula 1

--- nlw-habits/server/src/server.ts
+++ nlw-habits/server/src/server.ts
@@ -1,13 +1,29 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from "@prisma/client"
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
 /**
  * Metodo HTTP: Get, Post, Put, Patch, Delete
  */
 
-app.get('/', () => {
-    return 'Hello world'
+
+//Aqui vao as regras, para limitar quem de fato pode acessar as infromacoes do backend pela API => importante ver
+//quando for colocar em producao
+app.register(cors)
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Tomar'
+            }
+        }
+    })
+
+    return habits
 })
 
 app.listen({
