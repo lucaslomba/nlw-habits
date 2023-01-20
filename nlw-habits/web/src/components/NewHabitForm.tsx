3d2a30f2b62590db3e7fc71d1cc7353b8cf61490
lucaslomba <lucasazevedolomba@gmail.com>
2023-01-19 21:21:50 -0300

    Adicionando componente de checkbox

--- nlw-habits/web/src/components/NewHabitForm.tsx
+++ nlw-habits/web/src/components/NewHabitForm.tsx
@@ -1,4 +1,7 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+
+const avalaibleWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
 export function NewHabitForm(){
     return (
@@ -13,6 +16,24 @@ export function NewHabitForm(){
             />
 
             <label htmlFor="" className="font-semibold leading-tight mt-4">Qual a recorrência?</label>
+
+            <div className="mt-6 flex flex-col gap-3">
+                {
+                    avalaibleWeekDays.map((weekDay, index) => (
+                        <Checkbox.Root key={weekDay} className="flex items-center gap-3 group">
+                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                                <Checkbox.Indicator>
+                                    <Check size={20} className="text-white"/>
+                                </Checkbox.Indicator>
+                            </div>
+                            
+                            <span className="text-white leading-tight">
+                                {weekDay}
+                            </span>
+                        </Checkbox.Root>
+                    ))
+                }
+            </div>
 
             <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
                 <Check size={20} weight="bold"/>
